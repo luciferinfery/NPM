6d67bc4b214ff9b0415bd4f2d45ee8a92c2f4618
luciferinfery <rudfedor@gmail.com>
2024-01-22 15:08:45 +0200

    init project

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const sum = function (a, b) {
+  return a + b;
+};
+sum(2, 3);
+
+const minus = function (a, b) {
+  return a - b;
+};
+minus(3, 2);
+
+const mult = function (a, b) {
+  return a * b;
+};
+mult(3, 4);
